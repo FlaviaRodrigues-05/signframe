44930518bc449a2693d5e23e5fa88b265175a41a
Flavia <flaviavinrodrigues@gmail.com>
2026-09-26 20:03:50 +0530

    Backend Deployemnt Files

--- backend/app.py
+++ backend/app.py
@@ -465,9 +465,10 @@ def predict():
 # ============================================================
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
 
     app.run(
         host="0.0.0.0",
-        port=5001,
-        debug=True
+        port=port,
+        debug=False
     )
